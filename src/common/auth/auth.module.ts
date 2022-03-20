73fb72118d9ceba39b65c519e834be255080037c
VERHOVNA-RADA <62828748+VERHOVNA-RADA@users.noreply.github.com>
2022-03-20 16:37:08 +0100

    [feat] no overload matches this call

--- src/common/auth/auth.module.ts
+++ src/common/auth/auth.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthConfig } from '../../config/auth.config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersEntity } from '../../entities/users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersEntity])],
   providers: [AuthService, AuthConfig],
   controllers: [AuthController],
 })
